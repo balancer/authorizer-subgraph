19e4fab139dfa01cb02987d42e9ff4c4d75d2285
Tom French <tom@tomfren.ch>
2022-06-15 18:22:24 +0100

    refactor: create a helper function to calculate the permissionId

--- src/utils/actions.ts
+++ src/utils/actions.ts
@@ -1,5 +1,13 @@
 import { Bytes, crypto, store } from '@graphprotocol/graph-ts';
 import { Action, Permission } from '../types/schema';
+import { EVERYWHERE } from './constants';
+
+function getPermissionId(actionId: Bytes, account: Bytes): Bytes {
+  // To guarantee uniqueness, we copy the definition of permission IDs as used in the TimelockAuthorizer contract.
+  // https://github.com/balancer-labs/balancer-v2-monorepo/blob/e7cf9d269f520882782627012e936e93563ece5c/pkg/vault/contracts/authorizer/TimelockAuthorizer.sol#L236-L242
+  // TODO: Support `where`'s other than `EVERYWHERE`.
+  return crypto.keccak256(actionId.concat(account).concat(EVERYWHERE));
+}
 
 export function addAction(actionId: Bytes): void {
   const id = actionId.toHexString();
@@ -13,7 +21,7 @@ export function addAction(actionId: Bytes): void {
 }
 
 export function addPermission(actionId: Bytes, account: Bytes, txHash: Bytes): void {
-  const permissionId = crypto.keccak256(actionId.concat(account)).toHexString();
+  const permissionId = getPermissionId(actionId, account).toHexString();
 
   // Permissions are unique so we don't worry about collisions
   const permission = new Permission(permissionId);
@@ -25,6 +33,6 @@ export function addPermission(actionId: Bytes, account: Bytes, txHash: Bytes): v
 }
 
 export function removePermission(actionId: Bytes, account: Bytes): void {
-  const permissionId = crypto.keccak256(actionId.concat(account)).toHexString();
+  const permissionId = getPermissionId(actionId, account).toHexString();
   store.remove('Permission', permissionId);
 }
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,7 @@
-import { BigInt } from '@graphprotocol/graph-ts';
+import { BigInt, Bytes } from '@graphprotocol/graph-ts';
 
 export const bigZero = BigInt.fromI32(0);
 export const bigOne = BigInt.fromI32(1);
 export const AddressZero = '0x0000000000000000000000000000000000000000';
+
+export const EVERYWHERE = Bytes.fromHexString('0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF');
